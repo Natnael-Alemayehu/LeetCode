32a9c8d23df49d146e49c3b5bbe34d165ba04cb4
Natnael Alemayehu <se.natnael.alemayehu23@gmail.com>
2024-05-10 18:29:01 +0300

    Two sum is completed with the best solution

--- Two_Sum.py
+++ Two_Sum.py
@@ -2,9 +2,9 @@
 
 def main():
     l = [3,2,4]
-    print(twoSum(nums=l, target=6))
+    print(twoSum_n(nums=l, target=6))
 
-def twoSum(nums, target):
+def twoSum_n2(nums, target):
     output = []
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
@@ -12,4 +12,14 @@ def twoSum(nums, target):
                 output.append(i)
                 output.append(j)
     return output
+
+# This is the best solution for this problem. It uses hashmap which is pair_index in this solution
+def twoSum_n(nums,target):
+    pair_index = {}
+    
+    for i, num in enumerate(nums):
+        if target-num in pair_index:
+            return [i, pair_index[target-num]]
+        pair_index[num] = i
+    
 main()
